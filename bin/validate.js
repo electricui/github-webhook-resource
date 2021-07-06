6de5ec9598c757bf47caf26a27502e0bdc54082b
Michael Orenstein <michael@whiteshadows.me>
2021-07-06 21:22:00 +0930

    chore: Prettier pass

--- bin/validate.js
+++ bin/validate.js
@@ -1,81 +1,83 @@
-const Ajv = require('ajv');
+const Ajv = require("ajv");
 const ajv = new Ajv({ allErrors: true, jsonPointers: true });
-require('ajv-errors')(ajv);
-require('ajv-keywords')(ajv, 'transform');
+require("ajv-errors")(ajv);
+require("ajv-keywords")(ajv, "transform");
 
-const validOperations = ['create', 'delete'];
+const validOperations = ["create", "delete"];
 
 const envSchema = {
-    type: 'object',
-    properties: {
-        ATC_EXTERNAL_URL:    { type: 'string' },
-        BUILD_TEAM_NAME:     { type: 'string' },
-        BUILD_PIPELINE_NAME: { type: 'string' }
-    },
-    required: ['ATC_EXTERNAL_URL', 'BUILD_TEAM_NAME', 'BUILD_PIPELINE_NAME']
+  type: "object",
+  properties: {
+    ATC_EXTERNAL_URL: { type: "string" },
+    BUILD_TEAM_NAME: { type: "string" },
+    BUILD_PIPELINE_NAME: { type: "string" },
+  },
+  required: ["ATC_EXTERNAL_URL", "BUILD_TEAM_NAME", "BUILD_PIPELINE_NAME"],
 };
 
 const configSchema = {
-    type: 'object',
-    properties: {
-        source: {
-            type: 'object',
-            properties: {
-                github_api:   { type: 'string' },
-                github_token: { type: 'string' }
-            },
-            required: ['github_api', 'github_token']
-        },
-        params: {
-            type: 'object',
-            properties: {
-                org:           { type: 'string' },
-                repo:          { type: 'string' },
-                resource_name: { type: 'string' },
-                webhook_token: { type: 'string' },
-                events:        {
-                    type: 'array',
-                    items: {
-                        type: 'string',
-                        transform: ['trim', 'toLowerCase']
-                    }
-                },
-                operation:     {
-                    type: 'string',
-                    transform: ['trim', 'toEnumCase'],
-                    enum: validOperations,
-                    errorMessage: { enum: 'must be either create or delete' }
-                }
-            },
-            required: ['org', 'repo', 'resource_name', 'webhook_token', 'operation']
-        },
+  type: "object",
+  properties: {
+    source: {
+      type: "object",
+      properties: {
+        github_api: { type: "string" },
+        github_token: { type: "string" },
+      },
+      required: ["github_api", "github_token"],
     },
-    required: ['source', 'params']
+    params: {
+      type: "object",
+      properties: {
+        org: { type: "string" },
+        repo: { type: "string" },
+        resource_name: { type: "string" },
+        webhook_token: { type: "string" },
+        events: {
+          type: "array",
+          items: {
+            type: "string",
+            transform: ["trim", "toLowerCase"],
+          },
+        },
+        operation: {
+          type: "string",
+          transform: ["trim", "toEnumCase"],
+          enum: validOperations,
+          errorMessage: { enum: "must be either create or delete" },
+        },
+      },
+      required: ["org", "repo", "resource_name", "webhook_token", "operation"],
+    },
+  },
+  required: ["source", "params"],
 };
 
 function env(env) {
-    const test = ajv.compile(envSchema);
-    const isValid = test(env);
-    if (!isValid) fail(test);
+  const test = ajv.compile(envSchema);
+  const isValid = test(env);
+  if (!isValid) fail(test);
 }
 
 function config(config) {
-    const test = ajv.compile(configSchema);
-    const isValid = test(config);
-    if (!isValid) fail(test);
+  const test = ajv.compile(configSchema);
+  const isValid = test(config);
+  if (!isValid) fail(test);
 }
 
 function fail(test) {
-    let message = 'Invalid resource configuration:\n';
+  let message = "Invalid resource configuration:\n";
 
-    test.errors.forEach(err => {
-        message += `    ${err.dataPath.replace(/^\//, '')
-                                      .replace(/\//g, '.')
-                                      .replace(/.(\d+$)/, '[$1]')
-                        || 'root configuration'} ${err.message}\n`;
-    });
+  test.errors.forEach((err) => {
+    message += `    ${
+      err.dataPath
+        .replace(/^\//, "")
+        .replace(/\//g, ".")
+        .replace(/.(\d+$)/, "[$1]") || "root configuration"
+    } ${err.message}\n`;
+  });
 
-    throw new Error(message);
+  throw new Error(message);
 }
 
 module.exports = { env, config };
